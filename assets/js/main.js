042692c06f57da4c1ca29447f96a53204e94a74e
Bushido <70598205+Bushidonj@users.noreply.github.com>
2024-07-06 13:56:55 -0300

    test-image

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,10 +25,6 @@ gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.o
 gsap.from('.home__title', {opacity:0, duration: 3, delay: 1.3, y: 35, ease:'expo.out'});
 gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
 
-/*Scroll*/
-gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
-
-
 /*====== SCROLL REVEAL SECTION ======*/
 const sr = ScrollReveal({
     duration: 2500,
